42399fcac8fa200491bb56f1ccf1b4f4ff76353f
ls <1537515284@qq.com>
2022-04-14 07:23:26 +0800

    新增 在排序数组中查找元素的第一个和最后一个位置 算法题

---
+++ 中级算法/src/sortAndSearch/E在排序数组中查找元素的第一个和最后一个位置.java
@@ -0,0 +1,36 @@
+package sortAndSearch;
+
+import java.util.Arrays;
+
+public class E在排序数组中查找元素的第一个和最后一个位置 {
+    public static void main(String[] args) {
+        int[] nums = {5,7,7,8,8,10};
+        int target = 8;
+        int[] ints = (new E在排序数组中查找元素的第一个和最后一个位置()).searchRange(nums,target);
+        System.out.println(Arrays.toString(ints));
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        int leftIdx = binarySearch(nums, target, true);
+        int rightIdx = binarySearch(nums,target,false)-1;
+        if(leftIdx <= rightIdx && rightIdx < nums.length)
+            return new int[]{leftIdx,rightIdx};
+        return new int[]{-1,-1};
+    }
+
+
+    public int binarySearch(int[] nums, int target, boolean lower) {
+        int left = 0, right = nums.length - 1, ans = nums.length;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > target || (lower && nums[mid] >= target)) {
+                right = mid - 1;
+                ans = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+}
